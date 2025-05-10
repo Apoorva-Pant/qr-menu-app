7e0db39a6d14adbb5de6d628433e76022bd86460
Apoorva Pant <apoorvavijaypant13@gmail.com>
2025-05-11 00:07:21 +0530

    Updated Menu

--- app/page.jsx
+++ app/page.jsx
@@ -3,10 +3,13 @@ import MenuSection from '../components/MenuSection';
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold text-center my-4">
+     <h1 className="text-3xl font-bold text-center my-4 text-[#3b2f2f]">
   Welcome to Napster Coffee <span className="animate-bounce">☕</span><br />
-  <span className="text-xl font-normal">Pick your favorite brew today!</span>
+  <span className="text-xl font-normal text-[#5c4438]">
+    Pick your favorite brew today!
+  </span>
 </h1>
+
 
       <MenuSection />
     </div>
